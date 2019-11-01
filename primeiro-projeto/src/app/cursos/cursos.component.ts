3660501bd1a62d34dc2b76e6ee373fddf24f9414
designartt <designarttdev@gmail.com>
2019-11-01 18:22:32 -0300

    Aula 7

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,10 +10,20 @@ export class CursosComponent implements OnInit {
 
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'C#', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://google.com.br';
+
+    // for (let i = 0; i < this.cursos.length; i++) {
+    //   let curso = this.cursos[i];      
+    // }
+
+    
+
+    //var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
