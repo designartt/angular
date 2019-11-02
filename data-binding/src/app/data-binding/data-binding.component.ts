21ff6e008cfd09fe114716443323cb183eb6ee01
Luiz <designarttdev@gmail.com>
2019-11-02 13:09:53 -0300

    Aula 09

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string  = 'http://google.com.br';
+cursoAngular: boolean = true;
+urlImamge = 'https://droneshowla.com/wp-content/uploads/facebook-400x200.png'
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
